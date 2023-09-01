0785036b5c68127234183892924f23d061cbcb72
Ursule <133124119+UMikneviciute@users.noreply.github.com>
2023-09-01 13:28:56 +0300

    Dropdown menu added

--- javascript.js
+++ javascript.js
@@ -7,3 +7,10 @@ const toggleHero = () => {
 };
 
 setInterval(toggleHero, 8000);
+
+const dropdown = document.querySelector('.dropdown-list');
+const dropDownBtn = document.querySelector('.dropButton');
+
+dropDownBtn.addEventListener('click', () => {
+    dropdown.classList.toggle('hidden');
+});
